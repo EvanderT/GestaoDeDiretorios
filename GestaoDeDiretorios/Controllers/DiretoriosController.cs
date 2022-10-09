5c93a4f357a5696e29eabd68e6ab8a72b8092891
EvanderT <evanderdiasteixeira@gmail.com>
2022-10-09 09:13:57 +0100

    Criar e Eliminar pastas

---
+++ GestaoDeDiretorios/Controllers/DiretoriosController.cs
@@ -0,0 +1,89 @@
+﻿using GestaoDeDiretorios.Data;
+using GestaoDeDiretorios.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestaoDeDiretorios.Controllers
+{
+    public class DiretoriosController : Controller
+    {
+        // GET: Diretorios
+        private readonly AppDbContext _db = new AppDbContext();
+
+        public ActionResult Index()
+        {
+            var diretorios = _db.Diretorios.ToList();
+
+            return View(diretorios);
+        }
+
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Diretorio diretorio)
+        {
+            if (ModelState.IsValid)
+            {
+                // Caminho do Diretorio
+                var caminho = "~/Media/Users/" + diretorio.Nome;
+
+
+                // Criar diretório Físico
+                string folderFisico = Server.MapPath(caminho);
+                diretorio.Caminho = folderFisico;
+                
+
+                if (!Directory.Exists(folderFisico))
+                {
+                    _db.Diretorios.Add(diretorio);
+                    _db.SaveChanges();
+
+                    Directory.CreateDirectory(folderFisico);
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Mensagem = "Já existe uma pasta criada com o mesmo nome no mesmo local!";
+                }
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Eliminar(int id)
+        {
+            var diretorio = _db.Diretorios.Find(id);
+            return View(diretorio);
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(Diretorio viewModel)
+        {
+            var diretorio = _db.Diretorios.Find(viewModel.Id);
+
+            if (!Directory.Exists(diretorio.Caminho))
+            {
+                ViewBag.Mensagem = "Erro ao eliminar! O diretório não existe.";
+                return View(diretorio);
+            }
+            else
+            {
+                Directory.Delete(diretorio.Caminho);
+
+                _db.Diretorios.Remove(diretorio);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+        }
+    }
+}
---
+++ GestaoDeDiretorios/Migrations/202210090645472_first-migration.cs
@@ -0,0 +1,28 @@
+﻿namespace GestaoDeDiretorios.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Diretorios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Caminho = c.String(),
+                        DataCadastro = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Diretorios");
+        }
+    }
+}
